20d33b0966505ab50bbdc026626c8038b9c5034c
Timofey Solomko <tsolomko@gmail.com>
2026-02-12 11:49:10 +0800

    [swcomp] Add a new benchmark save file format
    Ultimately, it turned out that the approach of the old save file format of separating runs metadatas and results only leads to super complicated code. Unfortunately, it didn't help with append-updates to save files (they are still loaded fully, their content is modified in memory, and the actual file is then overwritten).

---
+++ Sources/swcomp/Benchmarks/SaveFile.swift
@@ -0,0 +1,27 @@
+// Copyright (c) 2026 Timofey Solomko
+// Licensed under MIT License
+//
+// See LICENSE for license information
+
+import Foundation
+
+struct SaveFile: Codable {
+
+    struct Run: Codable {
+
+        var uuid: UUID
+        var metadata: BenchmarkMetadata
+        var results: [BenchmarkResult]
+
+    }
+
+    var formatVersion = 2
+    var runs: [Run]
+
+    static func load(from path: String) throws -> SaveFile {
+        let decoder = JSONDecoder()
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        return try decoder.decode(SaveFile.self, from: data)
+    }
+
+}
